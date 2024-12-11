bb5b981b92a1adb65ffb3df500d9f49ef7fa967c
Ganesh Maulana <24083000068@student.unmer.ac.id>
2024-12-12 00:33:52 +0700

    Add files via upload

---
+++ MtkCanggih.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.g;
+
+/**
+ *
+ * @author Asus
+ */
+public class MtkCanggih extends Mtk {
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+
+    public double modulus(double a, double b) {
+        return a % b;
+    }
+
+
+    public double tambah(double a, double b, double c) {
+        return a + b + c;
+    }
+}
